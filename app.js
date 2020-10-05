7e9ddad0f0713b2f0f60d49f341f0d3235012784
Alina200207 <71718780+Alina200207@users.noreply.github.com>
2020-10-05 22:01:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var fs = require('fs');
-var string1 = fs.readFileSync('input.txt');
+const { decode } = require('punycode');
+const { error } = require('console');
+let string1;
+string1 = fs.readFileSync('input.txt');
+string1 = string1.toString();
 alph = new Array();
-for (let i = 0; i < string1.length; i++) {
+for (let i = 0; i < string1.Length; i++) {
     let b = string1.charAt(i);
     alph [b] = 0;
 }
-for (let i = 0; i < string1.length; i++) {
+for (let i = 0; i < string1.Length; i++) {
     let b = string1.charAt(i);
     alph [b]++;
 }
 let h = 0;
 let n = 0;
 for (let i in alph) {
-    alph[i] = alph[i] / string1.length;
+    alph[i] = alph[i] / string1.Length;
     n++
 }
 for (let i in alph) {
@@ -20,3 +24,5 @@ for (let i in alph) {
 }
 h = h * (-1);
 console.log(h);
+decode
+error
